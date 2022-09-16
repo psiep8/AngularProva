056ebded2391e1b116194a604d79ac50a427298f
psiep8 <82312159+psiep8@users.noreply.github.com>
2022-09-16 15:12:19 +0200

    First Commit

---
+++ src/app/button/button.component.ts
@@ -0,0 +1,28 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css']
+})
+export class ButtonComponent implements OnInit {
+
+  @Input() buttonConfig: MyButtonConfig;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
+
+export class MyButtonConfig {
+
+  customCssClass: string;
+
+  text: string;
+
+  icon: string;
+
+}
